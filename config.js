80a792b4c4c4c0ef2f1b48f63ab703859022fab7
tmlbl <tim.lebel@gmail.com>
2014-04-02 20:07:01 -0700

    remove config file from gitignore

---
+++ config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Tim on 4/2/14.
+ */
+exports.NEWRELIC_LICENSE_KEY='e59de094c674428493709dfd13af7766552461ee';
+exports.MODULUS_MONGODB='mongodb://cvince:e9azhx012@novus.modulusmongo.net:27017/mi4Myzeq';
+exports.CHAT_APP_SECRET='mysecretisyoursecret';
+exports.FB_APP_ID=1415482155376787;
+exports.FB_APP_SECRET='0494a9c872e7f47c83edb08d349d2ec0';
+exports.TW_APP_KEY='xIZx0gYClCCAZ9NAVUKkg';
+exports.TW_APP_SECRET='DPFYPRaMLWNRCkglTw28xEFqFpkXAFNghwefU9jhPrQ';
+exports.TRAVIS_TOKEN_AUTH='97KZBAikG3YgkkSoFWgulpUgGSTGFSzrI';
+exports.PORT=3000
